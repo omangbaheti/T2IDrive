ebe77610989e91772b3b22ed338af36997824472
Baheti <60500898+omangbaheti@users.noreply.github.com>
2025-06-18 15:48:34 -0700

    [A] HMI Assets Auto-update

--- Assets/UnityTechnologies/HMITemplate/Editor/TutorialInfo/Scripts/Readme.cs
+++ Assets/UnityTechnologies/HMITemplate/Editor/TutorialInfo/Scripts/Readme.cs
@@ -1,24 +1,16 @@
 ﻿using System;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-namespace HMI.Utilities
+public class Readme : ScriptableObject
 {
+    public Texture2D icon;
+    public string title;
+    public Section[] sections;
+    public bool loadedLayout;
 
-
-    public class Readme : ScriptableObject
+    [Serializable]
+    public class Section
     {
-        public Texture2D icon;
-        public string title;
-        public Section[] sections;
-        public bool loadedLayout;
-        public Scene[] scenes;
-
-        [Serializable]
-        public class Section
-        {
-            public string heading, text, linkText, url;
-        }
+        public string heading, text, linkText, url;
     }
-
 }
