904689ca9884ad2e8b6cb426ab881544d4698d8b
omangbaheti <omangbaheti@gmail.com>
2025-07-03 23:38:46 -0700

    Fixing Steering Wheel rotation error

--- Assets/SteeringWheelListener.cs
+++ Assets/SteeringWheelListener.cs
@@ -7,25 +7,29 @@ public class SteeringWheelListener : MonoBehaviour
     [SerializeField] List<LogitechControls> Inputs = new List<LogitechControls>();
     [SerializeField] LogitechAdapter logitechAdapter;
     [SerializeField] Transform steeringWheelAnchor;
-    [SerializeField] private float steeringInput;
+    [Range(0,1), SerializeField] private float steeringInput;
     private void Start()
     {
-        logitechAdapter.InputActionEvents[LogitechControls.Steering].AddListener(SteeringWheelInput);
+        // logitechAdapter.InputActionEvents[LogitechControls.Steering].AddListener(SteeringWheelInput);
+        
     }
 
     private void OnDestroy()
     {
-        logitechAdapter.InputActionEvents[LogitechControls.Steering].RemoveListener(SteeringWheelInput);
+        // logitechAdapter.InputActionEvents[LogitechControls.Steering].RemoveListener(SteeringWheelInput);
+    }
+
+    private void Update()
+    {
+        SteeringWheelInput(0);
     }
 
 
     private void SteeringWheelInput(float steeringWheel)
     {
-        Debug.Log(steeringWheel);
+        // Debug.Log(steeringWheel);
         steeringInput = steeringWheel;
-        Debug.Log(Mathf.Lerp(-300f, -300f, steeringWheel));
-        transform.rotation = Quaternion.AngleAxis(Mathf.Lerp(-300f, -300f, steeringWheel), steeringWheelAnchor.forward);
-        // transform.Rotate(steeringWheelAnchor.forward, Mathf.Lerp(-450f, -450f, steeringWheel));
-        // transform.RotateAround(steeringWheelAnchor.position, steeringWheelAnchor.forward, Mathf.Lerp(-450f, -450f, steeringWheel));
+        transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, Mathf.Lerp(450f, -450f, steeringWheel));
+
     }
 }
--- Assets/_Scripts/SteeringWheel/LogitechAdapter.cs
+++ Assets/_Scripts/SteeringWheel/LogitechAdapter.cs
@@ -49,17 +49,17 @@ public class LogitechAdapter : MonoBehaviour
     void Update()
     {
 
-        foreach (var control in drivingSimInputManager.ffbDevice.allControls)
-        {
-            if (control is AxisControl or InputControl<float>)
-            {
-                float value = drivingSimInputManager.GetFFBDeviceAxisValue(control.name);
-                if (InputScheme.TryGetValue(control.name, out LogitechControls inputValue))
-                {
-                    InputActions[InputScheme[control.name]].Invoke(value);
-                }
-            }
-        }
+        // foreach (var control in drivingSimInputManager.ffbDevice.allControls)
+        // {
+        //     if (control is AxisControl or InputControl<float>)
+        //     {
+        //         float value = drivingSimInputManager.GetFFBDeviceAxisValue(control.name);
+        //         if (InputScheme.TryGetValue(control.name, out LogitechControls inputValue))
+        //         {
+        //             InputActions[InputScheme[control.name]].Invoke(value);
+        //         }
+        //     }
+        // }
     }
 }
 
