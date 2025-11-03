092895b3a6d672f5fc67757d94f153ecc075ef36
omangbaheti <omangbaheti@gmail.com>
2025-11-03 08:49:52 -0800

    [C] Removing Unused Scripts

--- Assets/_Scripts/Scriptables/HPUILayoutSetup.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "HPUILayout", menuName = "HPUILayout", order = 0)]
-public class HPUILayoutSetup : ScriptableObject
-{
-    
-}
