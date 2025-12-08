b9294fe2dafbfd2fd216d7f6ba7549eca9a66418
vicon-user-ovi <vicon.user.ovi@gmail.com>
2025-12-08 13:07:13 -0800

    [C] Fixing speedometer

--- Assets/_Scripts/Speedometer.cs
+++ Assets/_Scripts/Speedometer.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 
 public class Speedometer : MonoBehaviour
 {
-    // private TextMeshPro speedometerTmp;
-    private TextMeshProUGUI speedometerTmpUgui;
+    [SerializeField] private TextMeshProUGUI speedometerTmpUgui;
     [SerializeField] private VehicleController vehicleController;
     
-    private void Awake()
+    private void OnEnable()
     {
-        // speedometerTmp = GetComponent<TextMeshPro>();
         speedometerTmpUgui = GetComponent<TextMeshProUGUI>();
     }
 
     private void Update()
     {
-        speedometerTmpUgui.text = vehicleController.CurrentSpeed.ToString("F1", CultureInfo.InvariantCulture) + " km/h"; ; 
+        speedometerTmpUgui.text = vehicleController.CurrentSpeed.ToString("F0", CultureInfo.InvariantCulture) + " km/h"; ; 
     }
 }
